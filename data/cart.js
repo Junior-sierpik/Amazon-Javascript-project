66f0eedb4148cd45f22b7dac7a98d539e6ec788d
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-16 17:07:39 +0200

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,24 @@
 export const cart = [];
+
+export function addToCart(productId){
+  let matchingItem;
+
+  cart.forEach((CartItem) => {
+    if (productId === CartItem.productId) {
+      matchingItem = CartItem;
+    }
+  });
+
+  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+  const quantity = Number(quantitySelector.value);
+
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId,
+      quantity: 1,
+      quantity
+    });
+  }
+}
