770051addd415f422964b55671faa6796acbfbaf
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-21 17:05:55 +0200

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="js-cart-item-container-${matchingProduct.id} cart-item-container">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -103,6 +103,9 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       const productId = link.dataset.productId;
       removeFromCart(productId);
-      console.log(cart);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`);
+      container.remove();
     });
   });
