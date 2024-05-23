987e909e2c864fadd265a21866f88f0598ea1b26
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-23 19:05:28 +0200

    Save new quantity to input

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -41,8 +41,9 @@ cart.forEach((cartItem) => {
           data-product-id="${matchingProduct.id}">
             Update
           </span>
-          <input class="quantity-input">
-          <span class="save-quantity-link link-primary">
+          <input class="quantity-input js-quantity-input-${matchingProduct.id}">
+          <span class="save-quantity-link link-primary js-save-link"
+          data-product-id="${matchingProduct.id}">
           Save
           </span>
           <span class="js-delete-link delete-quantity-link link-primary" data-product-id="${matchingProduct.id}">
@@ -139,4 +140,15 @@ document.querySelectorAll('.js-update-link').forEach((link) => {
   });
 });
 
+document.querySelectorAll('.js-save-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
 
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+
+    container.classList.remove('is-editing-quantity');
+
+    const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
+    const newQuantity = Number(quantityInput.value);
+  });
+})
