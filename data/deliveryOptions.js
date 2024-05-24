1b879f06381414a519c209ff4e3b3b3619db2ff8
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-24 13:42:39 +0200

    Save delivery options to data

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+const deliveryOptions = [{
+  id: '1',
+  deliveryDays: 7,
+  prizeCents: 0,
+}, {
+  id: '2',
+  deliveryDays: 3,
+  prizeCents: 499,
+}, {
+  id: '3',
+  deliveryDays: 1,
+  prizeCents: 999
+}];
