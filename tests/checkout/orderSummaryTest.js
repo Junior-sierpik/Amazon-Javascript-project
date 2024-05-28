cf78bb5673d6b5afa9d4108fdb421cad16950fb1
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-05-28 23:25:06 +0200

    Check if the product price are displayed correctly on the page

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -52,6 +52,12 @@ describe('Test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-name-${productId2}`).innerText
     ).toEqual('Intermediate Size Basketball');
+    expect(
+      document.querySelector(`.js-product-price-${productId1}`).innerText
+    ).toEqual('$10.90');
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual('$20.95');
   });
 
   it('removes a product', () => {
@@ -65,6 +71,9 @@ describe('Test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-name-${productId2}`).innerText
     ).toEqual('Intermediate Size Basketball');
+    expect(
+      document.querySelector(`.js-product-price-${productId2}`).innerText
+    ).toEqual('$20.95');
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
   });
