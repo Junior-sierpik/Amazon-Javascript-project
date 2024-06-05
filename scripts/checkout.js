5dd60cc8c1fc7f931f5391cf384f08612400c54a
Junior-sierpik <sierpinski.kamil@wp.pl>
2024-06-05 22:59:32 +0200

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,24 @@ import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js';
 
+async function loadPage(){ 
+
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+  renderCheckoutHeader();
+  renderPaymentSummary();
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage()
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
@@ -19,7 +37,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
-
+*/
 /*
 loadProducts(() => {
   renderCheckoutHeader();
